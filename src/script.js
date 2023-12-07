7131dd501e23edece3551c719c1cd516d478561b
KelfinTDetonator <kelfinantoni@gmail.com>
2023-12-07 20:43:41 +0700

    chore: rewrite comments

--- src/script.js
+++ src/script.js
@@ -1,30 +1,30 @@
 const rectangles = document.querySelector(".rectangles");
 const root = document.querySelector(":root");
-
 let totalRectangles;
 
 // slider
 const slider = document.getElementById("rangeSize");
 const output = document.getElementById("output");
-output.textContent = slider.value;
+
 slider.addEventListener("input", function () {
   output.textContent = this.value;
 });
+// when there is changes in the input element
 slider.addEventListener("change", function () {
   const divs = document.querySelectorAll(".col");
-  divs.forEach((div) => { // kalo udah ada div dengan class col -> remove semua elemen sebelum nanti di append lagi
+  divs.forEach((div) => { // if there are divs with .col -> remove all elements before the element got appended to the div
     if (div) { div.remove(); } // reset div
   });
-  totalRectangles = this.value; // setiap ada perubahan pada slider, totalRectangles di-assign ulang
-  root.style.setProperty("--grid", `${totalRectangles}`); // buat set value ke variable css
-  for (let index = 0; index < totalRectangles; index++) {
+  totalRectangles = this.value; // each time there is change in slider, totalRectangles got re-assign
+  root.style.setProperty("--grid", `${totalRectangles}`); // set the value to the css var
+  for (let index = 0; index < totalRectangles; index++) { // 2D looping since there are rows and columns
     const div = document.createElement("div");
     div.classList.toggle("col");
-    rectangles.append(div);
+    rectangles.append(div); // while looping, append div element (row) to the rectangles div container
     for (let j = 0; j < totalRectangles; j++) {
       const div2 = document.createElement("div");
       div2.classList.toggle("col");
-      rectangles.append(div2);
+      rectangles.append(div2); // while looping, append div element (column) to the rectangles div container
     }
   }
 });
